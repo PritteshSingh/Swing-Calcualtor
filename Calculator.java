c9d016d5801a749d116d4600497618ab1e2754c8
Prittesh Singh <pritteshsingh@gmail.com>
2022-04-20 20:44:16 +0530

    adding source file

---
+++ Calculator.java
@@ -0,0 +1,149 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Calculator implements ActionListener {
+    private static JTextField inputBox;
+
+    Calculator(){}
+    public static void main(String[] args) {
+        createWindow();   //declare window method
+    }
+
+    private static void createWindow() {
+        JFrame frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        createUI(frame);
+        frame.setSize(200, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private static void createUI(JFrame frame) {
+        JPanel panel = new JPanel();
+        Calculator calculator = new Calculator();
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        panel.setLayout(layout);
+
+        inputBox = new JTextField(10);
+        inputBox.setEditable(false);
+
+        //number buttons
+        JButton button0 = new JButton("0");
+        JButton button1 = new JButton("1");
+        JButton button2 = new JButton("2");
+        JButton button3 = new JButton("3");
+        JButton button4 = new JButton("4");
+        JButton button5 = new JButton("5");
+        JButton button6 = new JButton("6");
+        JButton button7 = new JButton("7");
+        JButton button8 = new JButton("8");
+        JButton button9 = new JButton("9");
+
+        //operation buttons
+        JButton buttonPlus = new JButton("+");
+        JButton buttonMinus = new JButton("-");
+        JButton buttonDivide = new JButton("/");
+        JButton buttonMultiply = new JButton("x");
+        JButton buttonClear = new JButton("C");
+        JButton buttonDot = new JButton(".");
+        JButton buttonEquals = new JButton("=");
+
+        //on click action
+        button1.addActionListener(calculator);
+        button2.addActionListener(calculator);
+        button3.addActionListener(calculator);
+        button4.addActionListener(calculator);
+        button5.addActionListener(calculator);
+        button6.addActionListener(calculator);
+        button7.addActionListener(calculator);
+        button8.addActionListener(calculator);
+        button9.addActionListener(calculator);
+        button0.addActionListener(calculator);
+
+        buttonPlus.addActionListener(calculator);
+        buttonMinus.addActionListener(calculator);
+        buttonDivide.addActionListener(calculator);
+        buttonMultiply.addActionListener(calculator);
+        buttonClear.addActionListener(calculator);
+        buttonDot.addActionListener(calculator);
+        buttonEquals.addActionListener(calculator);
+
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0; gbc.gridy = 0; panel.add(button1, gbc);
+        gbc.gridx = 1; gbc.gridy = 0; panel.add(button2, gbc);
+        gbc.gridx = 2; gbc.gridy = 0; panel.add(button3, gbc);
+        gbc.gridx = 3; gbc.gridy = 0; panel.add(buttonPlus, gbc);
+        gbc.gridx = 0; gbc.gridy = 1; panel.add(button4, gbc);
+        gbc.gridx = 1; gbc.gridy = 1; panel.add(button5, gbc);
+        gbc.gridx = 2; gbc.gridy = 1; panel.add(button6, gbc);
+        gbc.gridx = 3; gbc.gridy = 1; panel.add(buttonMinus, gbc);
+        gbc.gridx = 0; gbc.gridy = 2; panel.add(button7, gbc);
+        gbc.gridx = 1; gbc.gridy = 2; panel.add(button8, gbc);
+        gbc.gridx = 2; gbc.gridy = 2; panel.add(button9, gbc);
+        gbc.gridx = 3; gbc.gridy = 2; panel.add(buttonDivide, gbc);
+        gbc.gridx = 0; gbc.gridy = 3; panel.add(buttonDot, gbc);
+        gbc.gridx = 1; gbc.gridy = 3; panel.add(button0, gbc);
+        gbc.gridx = 2; gbc.gridy = 3; panel.add(buttonClear, gbc);
+        gbc.gridx = 3; gbc.gridy = 3; panel.add(buttonMultiply, gbc);
+        gbc.gridwidth = 3;
+
+        gbc.gridx = 0; gbc.gridy = 4; panel.add(inputBox, gbc);
+        gbc.gridx = 3; gbc.gridy = 4; panel.add(buttonEquals, gbc);
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+
+    }
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        if (command.charAt(0) == 'C') {
+            inputBox.setText("");
+        }else if (command.charAt(0) == '=') {
+            inputBox.setText(evaluate(inputBox.getText()));
+        }else {
+            inputBox.setText(inputBox.getText() + command);
+        }
+    }
+
+    public static String evaluate(String expression) {
+        char[] arr = expression.toCharArray();
+        StringBuilder operand1 = new StringBuilder();
+        StringBuilder operand2 = new StringBuilder();
+        StringBuilder operator = new StringBuilder();
+        double result;
+
+        for (char c : arr) {
+            if (c >= '0' && c <= '9' || c == '.') {
+                if (operator.length() == 0) {
+                    operand1.append(c);
+                } else {
+                    operand2.append(c);
+                }
+            }
+
+            if (c == '+' || c == '-' || c == '/' || c == '*') {
+                operator.append(c);
+            }
+        }
+
+        switch (operator.toString()) {
+            case "+":
+                result = (Double.parseDouble(operand1.toString()) + Double.parseDouble(operand2.toString()));
+                break;
+            case "-":
+                result = (Double.parseDouble(operand1.toString()) - Double.parseDouble(operand2.toString()));
+                break;
+            case "/":
+                result = (Double.parseDouble(operand1.toString()) / Double.parseDouble(operand2.toString()));
+                break;
+            default:
+                result = (Double.parseDouble(operand1.toString()) * Double.parseDouble(operand2.toString()));
+                break;
+        }
+        return operand1 + operator.toString() + operand2 + "=" +result;
+    }
+
+}
